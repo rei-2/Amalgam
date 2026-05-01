9a4a812082cd1c224ac475c975ac1e6b3194d70b
nocw <119352023+nocw@users.noreply.github.com>
2026-05-02 00:49:53 +0300

    Update DllMain.cpp

--- Amalgam/src/DllMain.cpp
+++ Amalgam/src/DllMain.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <tlhelp32.h>
 #include "Core/Core.h"
 #include "Utils/ExceptionHandler/ExceptionHandler.h"
 
@@ -15,13 +16,60 @@ DWORD WINAPI MainThread(LPVOID lpParam)
 	FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), EXIT_SUCCESS);
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+__int32 __stdcall DllMain(HMODULE This_Module, unsigned __int32 Call_Reason, void* Reserved)
 {
-	if (fdwReason == DLL_PROCESS_ATTACH)
+	if (Call_Reason == DLL_PROCESS_DETACH)
 	{
-		if (const auto hThread = CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr))
-			CloseHandle(hThread);
+		__fastfail(EXIT_SUCCESS);
 	}
+	else
+	{
+		if (Call_Reason == DLL_PROCESS_ATTACH)
+		{
+			if (GetModuleHandleW(L"tf_win64.exe") == nullptr)
+			{
+				void* Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	return TRUE;
+				PROCESSENTRY32W Snapshot_Entry;
+
+				Snapshot_Entry.dwSize = sizeof(Snapshot_Entry);
+
+				Traverse_Snapshot_Label:
+				{
+					if (Process32NextW(Snapshot, &Snapshot_Entry) == 0)
+					{
+						return 1;
+					}
+
+					if (wcscmp(Snapshot_Entry.szExeFile, L"tf_win64.exe") != 0)
+					{
+						goto Traverse_Snapshot_Label;
+					}
+				}
+
+				void* Process = OpenProcess(PROCESS_ALL_ACCESS, 0, Snapshot_Entry.th32ProcessID);
+
+				void* Remote_Path = VirtualAllocEx(Process, nullptr, 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+				wchar_t Local_Path[MAX_PATH];
+
+				GetModuleFileNameW(This_Module, Local_Path, sizeof(Local_Path));
+
+				WriteProcessMemory(Process, Remote_Path, Local_Path, sizeof(Local_Path), nullptr);
+
+				WaitForSingleObject(CreateRemoteThread(Process, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, Remote_Path, 0, nullptr), INFINITE);
+
+				VirtualFreeEx(Process, Remote_Path, 0, MEM_RELEASE);
+			}
+			else
+			{
+				if (const HANDLE Main_Thread = CreateThread(nullptr, 0, MainThread, This_Module, 0, nullptr))
+				{
+					CloseHandle(Main_Thread);
+				}
+			}
+		}
+	}
+	
+	return 1;
 }
